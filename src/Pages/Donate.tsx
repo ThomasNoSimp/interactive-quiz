731df1e0da476fc146b8e7d497568eaec3c65877
ThomasNoSimp <thurawaizin.conceptx@gmail.com>
2024-08-15 11:35:09 +0630

    Added a TODO comment

--- src/Pages/Donate.tsx
+++ src/Pages/Donate.tsx
@@ -6,7 +6,7 @@ const DonatePage = () => {
     return (
         <div className="container">
             <button className="donateButton">
-                <Link className="donateLinkReact" to="#donate">Donate</Link>
+                <Link className="donateLinkReact" to="#donate">Donate</Link> // TODO: Handle donating action.
             </button>
             <p><b>We appreciate your donation. &lt;3</b></p>
         </div>
